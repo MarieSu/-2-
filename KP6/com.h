db24f81af927631b63bc500468e35286b1b4a186
MarieSu <100448518+MarieSu@users.noreply.github.com>
2022-04-16 01:21:58 +0300

    Add files via upload

---
+++ KP6/com.h
@@ -0,0 +1,16 @@
+#ifndef com_h
+#define com_h
+
+typedef struct
+{
+    char surname[50];
+    int nproc;
+    char tproc[50];
+    int sram;
+    char tdisk[50];
+    int ndisk;
+    int cdisk;
+    int ndev;
+} com;
+
+#endif // com_h
